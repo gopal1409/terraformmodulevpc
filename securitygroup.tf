73e8b583604c14913f2fadfcaa168e82e7755e68
gopal <gopal1409@gmail.com>
2021-10-11 02:05:50 -0700

    security group

---
+++ securitygroup.tf
@@ -0,0 +1,15 @@
+#aws security group terraform module
+module "public_bastion_sg" {
+   
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "3.18.0"
+  # insert the 3 required variables here
+  name = "public-bastion-sg"
+  description = "this security group for ec2"
+  vpc_id = module.vpc.vpc_id
+  #ingress rules
+  ingress-rules = ["ssh-tcp","http-80-tcp"]
+  ingress_cidr_blocks = [module.vpc.vpc_cidr_block]
+  egress_rules = ["all-all"]
+  tags = local.common_tags
+}
