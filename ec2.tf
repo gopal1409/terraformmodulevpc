65801f8f93ccd643f8f81269ee2f7020d21fee33
gopal <gopal1409@gmail.com>
2021-10-11 02:33:50 -0700

    ec2 sg and vpc module

--- ec2.tf
+++ ec2.tf
@@ -4,12 +4,12 @@ module "ec2-public" {
   # insert the 34 required variables here
   name = "${var.environment}-BastionHost"
   #instance_count = 5
-  ami = 
+  ami = data.aws_ami.amzlinux2.id
   instance_type = var.instance_type
   #key_name = var.instance_key
   #monitoring = true
   subnet_id = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [module.public_bastion_sg.this_security_group]
+  vpc_security_group_ids = [module.public_bastion_sg.this_security_group_id]
   tags = local.common_tags
 
 }
